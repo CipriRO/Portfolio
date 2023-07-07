533dd5213b449955a1a2bb49e1dccf7a750571a5
Ciprian <stefanciprian444@gmail.com>
2023-07-07 13:19:27 +0300

    Added a new project

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ import clothingStore3 from "./assets/images/clothing-store-3.jpg";
 import clothingStore4 from "./assets/images/clothing-store-4.jpg";
 import clothingStore5 from "./assets/images/clothing-store-5.jpg";
 import clothingStore6 from "./assets/images/clothing-store-6.jpg";
+import todoApp1 from "./assets/images/todo-app-1.jpg";
+import todoApp2 from "./assets/images/todo-app-2.jpg";
+import todoApp3 from "./assets/images/todo-app-3.jpg";
 import ViewProject from "./components/viewProject";
 import { AnimatePresence } from "framer-motion";
 import ContactMe from "./components/ContactMe";
@@ -46,6 +49,18 @@ function App() {
       linkName: "eliconstruction.co.uk",
       images: [eliasProject3, eliasProject2, eliasProject1],
       badge: {badgeType: 'badge-success', badgeContent: 'Online', badgePosition: {l: '80%', t: '3.8%'}}
+    },
+    {
+      name: "Todo App",
+      duration: "2 days",
+      date: "07.07.2023",
+      description: `This project is a todo list app that lets you create, delete and complete your tasks with ease. It is built with HTML, CSS, JS, React.js, Tailwind, DaisyUI and Framer Motion. It has a beautiful design that follows the principles of minimalism and usability. It also has animations that make the app more interactive and fun to use. You can add a new task by typing in the input field and clicking 'Add'. You can delete a task by clicking on the trash icon. You can mark a task as completed by clicking on the check icon.`,
+      images: [
+        todoApp3,
+        todoApp2,
+        todoApp1
+      ],
+      badge: {badgeType: 'badge-warning', badgeContent: 'New', badgePosition: {l: '83%', t: '3.8%'}}
     },
   ];
 
